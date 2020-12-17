92649e3d2b888c7ee6ea84da881c0a8bc5de21b8
zinirun <heojj97@naver.com>
2020-12-17 20:23:26 +0900

    Add Proxy

--- redux-middleware/src/api/posts.js
+++ redux-middleware/src/api/posts.js
@@ -1,35 +1,11 @@
-// Fake API
-
-const sleep = (n) => new Promise((resolve, reject) => setTimeout(resolve, n));
-
-// {id, title, body}
-
-const posts = [
-  {
-    id: 1,
-    title: "redux 미들웨어 배우기1",
-    body: "redux 미들웨어를 만들어봐요1",
-  },
-
-  {
-    id: 2,
-    title: "redux 미들웨어 배우기2",
-    body: "redux 미들웨어를 만들어봐요2",
-  },
-
-  {
-    id: 3,
-    title: "redux 미들웨어 배우기3",
-    body: "redux 미들웨어를 만들어봐요3",
-  },
-];
+import axios from "axios";
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get("/posts");
+  return response.data;
 };
 
 export const getPostById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`/posts/${id}`);
+  return response.data;
 };
